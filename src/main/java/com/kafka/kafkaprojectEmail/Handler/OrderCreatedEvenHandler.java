49f34bcf81bd729368a06ab7d9f5bf1174d5759d
satyendrashiroya07 <shiroya0749@gmail.com>
2026-04-19 18:12:41 +0530

    Created Payment Service and accepting to consume payment producer and send mail after payment done/fail, sending mail order-created if payment success and order-failed if payment failed and also for the product we are rolling back if payment failed we are not reducing the product

--- src/main/java/com/kafka/kafkaprojectEmail/Handler/OrderCreatedEvenHandler.java
+++ src/main/java/com/kafka/kafkaprojectEmail/Handler/OrderCreatedEvenHandler.java
@@ -1,46 +1,46 @@
-package com.kafka.kafkaprojectEmail.Handler;
-
-import com.kafka.kafkaprojectEmail.EmailServices.EmailService;
-import jakarta.transaction.Transactional;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.kafka.annotation.KafkaHandler;
-import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.support.KafkaHeaders;
-import org.springframework.messaging.handler.annotation.Header;
-import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.stereotype.Component;
-import shiroya.orderEvent.OrderEvent;
-
-@Component
-@KafkaListener(topics="order-created")
-public class OrderCreatedEvenHandler {
-
-    public final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
-    private EmailService emailService;
-
-    public OrderCreatedEvenHandler(EmailService emailService) {
-        this.emailService = emailService;
-    }
-
-    @Transactional
-    @KafkaHandler
-    public void handle(@Payload OrderEvent orderCreatedEvent,
-                       @Header(KafkaHeaders.RECEIVED_KEY) String messagekey) {
-	LOGGER.info("Received a new event: " + orderCreatedEvent.getOrderId() + " with productId: " + orderCreatedEvent.getProductId());
-
-    String emailBody = "Order Details:\n" +
-            "OrderId: " + orderCreatedEvent.getOrderId() + "\n" +
-            "ProductId: " + orderCreatedEvent.getProductId() + "\n" +
-            "UserId: " + orderCreatedEvent.getUserId()+ "\n" +
-            "Quantity: " + orderCreatedEvent.getQuantity()+ "\n" +
-            "Status: " + orderCreatedEvent.getStatus();
-
-    System.out.println("Email: " + orderCreatedEvent.getEmail());
-        emailService.sendEmail(orderCreatedEvent.getEmail(),
-                "New Order Created",
-                emailBody
-        );
-
-    }
-}
+//package com.kafka.kafkaprojectEmail.Handler;
+//
+//import com.kafka.kafkaprojectEmail.EmailServices.EmailService;
+//import jakarta.transaction.Transactional;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.kafka.annotation.KafkaHandler;
+//import org.springframework.kafka.annotation.KafkaListener;
+//import org.springframework.kafka.support.KafkaHeaders;
+//import org.springframework.messaging.handler.annotation.Header;
+//import org.springframework.messaging.handler.annotation.Payload;
+//import org.springframework.stereotype.Component;
+//import shiroya.orderEvent.OrderEvent;
+//
+//@Component
+//@KafkaListener(topics="order-created")
+//public class OrderCreatedEvenHandler {
+//
+//    public final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
+//    private EmailService emailService;
+//
+//    public OrderCreatedEvenHandler(EmailService emailService) {
+//        this.emailService = emailService;
+//    }
+//
+//    @Transactional
+//    @KafkaHandler
+//    public void handle(@Payload OrderEvent orderCreatedEvent,
+//                       @Header(KafkaHeaders.RECEIVED_KEY) String messagekey) {
+//	LOGGER.info("Received a new event: " + orderCreatedEvent.getOrderId() + " with productId: " + orderCreatedEvent.getProductId());
+//
+//    String emailBody = "Order Details:\n" +
+//            "OrderId: " + orderCreatedEvent.getOrderId() + "\n" +
+//            "ProductId: " + orderCreatedEvent.getProductId() + "\n" +
+//            "UserId: " + orderCreatedEvent.getUserId()+ "\n" +
+//            "Quantity: " + orderCreatedEvent.getQuantity()+ "\n" +
+//            "Status: " + orderCreatedEvent.getStatus();
+//
+//    System.out.println("Email: " + orderCreatedEvent.getEmail());
+//        emailService.sendEmail(orderCreatedEvent.getEmail(),
+//                "New Order Created",
+//                emailBody
+//        );
+//
+//    }
+//}
